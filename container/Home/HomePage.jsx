b9092b66a374063d8428d78623c62a108f194526
Harsh Raj <saketharshraj@gmail.com>
2022-03-10 00:52:07 +0530

    Changed Template test Button name

--- container/Home/HomePage.jsx
+++ container/Home/HomePage.jsx
@@ -19,7 +19,7 @@ function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
-      <button onClick={handleSomeAPIOperation}>Test</button>
+      <button onClick={handleSomeAPIOperation}>Test API</button>
       <Testomonial />
     </div>
   )
