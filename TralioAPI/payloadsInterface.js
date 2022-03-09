db842793c0e5b7f34cc31df7f606922a6341d111
Harsh Raj <saketharshraj@gmail.com>
2022-03-10 00:26:22 +0530

    User Payload format

---
+++ TralioAPI/payloadsInterface.js
@@ -0,0 +1,12 @@
+const userPayloadInterface = {
+    username: "",
+    firstname: "",
+    lastname: "",
+    userEmail: "",
+    userPassword: "",
+    userDOB: "",
+    userGender: "",
+    userCountry: "",
+}
+
+export default userPayloadInterface;
