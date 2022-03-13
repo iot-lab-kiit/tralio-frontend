9daedb789f245d56f64b3c55f944cea1aedcf9c3
Harsh Raj <saketharshraj@gmail.com>
2022-03-13 18:55:02 +0530

    user static path connected to Tralio API

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const localAPI = 'http://localhost:3000';
+const localAPI = 'http://localhost:8000';
 const prodAPI = 'https://tralio-api.herokuapp.com';
 
 module.exports = {
--- pages/posts/[user]/index.js
+++ pages/posts/[user]/index.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from "react";
+import getConfig from "next/config";
+const { publicRuntimeConfig } = getConfig();
+const { API_URL, apiVersion } = publicRuntimeConfig;
 
 function PostByUser(props) {
   return (
@@ -6,49 +9,34 @@ function PostByUser(props) {
       <div>Hello</div>
       <h1>{props.posts}</h1>
     </>
-    
-  )
+  );
 }
 
 export async function getStaticPaths() {
-    // const paths = await fetch('https://jsonplaceholder.typicode.com/users')
-    // const users = await paths.json()
-    // const pathsUsers = users.map(user => ({
-    //     params: {
-    //         user: user.id
-    //     }
-    // }))
-    const paths = [
-        {
-            params: {
-                user: 'user-1'
-            },
-        },
-        {
-            params: {
-                user: 'user-2'
-            },
-        },
-        {
-            params: {
-                user: 'user-3'
-            },
-        },
-    ]
-    return {
-        paths: paths,
-        fallback: false
-    }
+
+  const response = await fetch(`${API_URL}/api/${apiVersion}/users/all-users`);
+
+  const userData = await response.json();
+  const paths = userData.users;
+
+  const pathsUsers = paths.map((user) => ({
+    params: {
+      user: user.username ,
+    },
+  }));
+
+  return {
+    paths: pathsUsers,
+    fallback: false,
+  };
 }
 
 export async function getStaticProps() {
-   
-
-    return {
-        props: {
-            posts: "Successfull Testing user posts",
-        },
-    };
+  return {
+    props: {
+      posts: "Successfull Testing user posts",
+    },
+  };
 }
 
-export default PostByUser
+export default PostByUser;
