afedd1a58b2420689ec3aab5e15bd86cb8ba0a0c
roshanchatei <rkc.mj5@gmail.com>
2022-03-14 15:38:01 +0530

    Navbar components done

---
+++ components/Navbar/LandingNavbar.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useRef, useState } from 'react';
+import AppBar from "@mui/material/AppBar";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from "@mui/material/Container";
+import Link from "../Layouts/Link"
+import Logo from "/public/images/Logos/logo.svg"
+import Image from "next/image";
+
+const LandingNavbar = () => {
+
+    // const [onTop, setOnTop] = useState(true);
+    // const navRef = useRef();
+    //
+    // useEffect(() => {
+    //     const position = navRef.current.offsetTop;
+    //     window.onscroll = function () {
+    //         if (window.pageYOffset > position) {
+    //             setOnTop(false);
+    //         } else {
+    //             setOnTop(true);
+    //         }
+    //     };
+    // });
+
+    return (
+        <>
+            <AppBar
+                color={'transparent'}
+                elevation={0}
+                position="absolute"
+            >
+                <Container maxWidth={'lg'}>
+                    <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'} pt={1} pb={1}>
+                        <Box component={Link} noLinkStyle href="/" width={'120px'} height={'41px'}>
+                            <Image src={Logo} alt={'logo'} />
+                        </Box>
+                        <Button variant={'contained'}>
+                            Login
+                        </Button>
+                    </Box>
+                </Container>
+            </AppBar>
+        </>
+    );
+};
+
+export default LandingNavbar;
--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,7 +1,61 @@
-function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
-}
+import React, { useEffect, useRef, useState } from 'react';
+import AppBar from "@mui/material/AppBar";
+import Box from '@mui/material/Box';
+import Hidden from '@mui/material/Hidden';
+import IconButton from '@mui/material/IconButton';
+import Avatar from "@mui/material/Avatar";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+const Navbar = () => {
+
+    const [onTop, setOnTop] = useState(true);
+    const navRef = useRef();
+
+    useEffect(() => {
+        const position = navRef.current.offsetTop;
+        window.onscroll = function () {
+            if (window.pageYOffset > position) {
+                setOnTop(false);
+            } else {
+                setOnTop(true);
+            }
+        };
+    });
+
+    return (
+        <>
+            <AppBar
+                color={'transparent'}
+                ref={navRef}
+                elevation={!onTop ? 4 : 0}
+                position="fixed"
+            >
+                <Box
+                    display={'flex'}
+                    justifyContent={'space-between'}
+                    alignItems={'center'}
+                    p={1}
+                    bgcolor={!onTop ? '#317CEB' : ''}
+                    color={!onTop ? '#FFF' : '#317CEB'}
+                >
+                    <Box display={'flex'} alignItems={'center'}>
+                        <IconButton>
+                            <ArrowBackIcon />
+                        </IconButton>
+                        <Hidden mdDown>
+                            <Box ml={1} fontSize={'20px'}>
+                                Back to Dashboard
+                            </Box>
+                        </Hidden>
+                    </Box>
+                    <Avatar>K</Avatar>
+                </Box>
+            </AppBar>
+        </>
+    );
+};
+
+
+
 
 export default Navbar;
