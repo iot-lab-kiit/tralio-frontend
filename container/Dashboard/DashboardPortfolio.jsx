bcbdb709da1492474c361bf5deff047af7564b23
roshanchatei <rkc.mj5@gmail.com>
2022-03-18 22:28:53 +0530

    Deployment check

--- container/Dashboard/DashboardPortfolio.jsx
+++ container/Dashboard/DashboardPortfolio.jsx
@@ -5,11 +5,9 @@ import BackgroundSearch from "../../components/Dashboard/BackgroundSearch";
 
 export default function DashboardPortfolio() {
 
-    const [currentStage , setCurrentStage] = useState(0);
-
     return (
         <>
-            <BackgroundSearch color={'#fff'} head={'Time and time again, Mosh\'s students praise his ability to break down complex topics into simple, digestible'} src={'/backgrounds/dashboardBg6.jpg'} />
+            <BackgroundSearch color={'#fff'} head={'Time and time again, Moshes students praise his ability to break down complex topics into simple, digestible'} src={'/backgrounds/dashboardBg3.jpg'} />
             <Container maxWidth={'lg'}>
                 Dashboard portfolio
             </Container>
