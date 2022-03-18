af1d8fbb6ca17bb0eede484e4e3e7aa598b33c21
Sibananda Sahu <sibananda6481@gmail.com>
2022-03-18 20:04:20 +0530

    testimonial component

--- components/Testimonial/Testimonial.jsx
+++ components/Testimonial/Testimonial.jsx
@@ -1,6 +1,22 @@
+import Image from 'next/image'
+import dp from '../../public/images/Testimonial/dp.svg'
+import styles from './Testimonial.module.css'
+
+
 function Testomonial() {
   return (
-    <div>Testomonial Design</div>
+    <div>
+      <div className={styles.box}>
+        <div className={styles.img}>
+          <Image src={dp} alt="Pic" />
+        </div>
+        <div className={styles.content}>
+          <p>Our dedicated patient engagement app and web portal allow you to access information.portal allow you to access information.</p>
+          <h5>NAME</h5>
+          <h4>CEO OF TRALIO</h4>
+        </div>
+      </div>
+    </div>
   )
 }
 
