20d6eb2ba9ec4479fdd279b1e242c8f4888cd150
Harsh Raj <saketharshraj@gmail.com>
2022-03-21 14:25:57 +0530

    Imports conventions

--- container/Home/HomePage.jsx
+++ container/Home/HomePage.jsx
@@ -3,22 +3,19 @@ import { useState } from "react";
 import styles from "./HomePage.module.css";
 
 // Components
+import Carousel from "../../components/Carousel/Carousel";
 import Pop from "../../components/LoginSignupPop/LoginSignupPop";
-import DetailFeatures from "../../components/DetailFeatures/DetailFeatures";
 import DetailSummary from "../../components/DetailSummary/DetailSummary";
+import DetailFeatures from "../../components/DetailFeatures/DetailFeatures";
 import TopLandingScreen from "../../components/LandingPage/TopLandingScreen";
 
 // Data Utilities
 import homeDetailSummary from "../../TralioAPI/DetailSummary";
 import homeDetailFeature from "../../TralioAPI/homeDetailFeature";
 
-import Carousel from "../../components/Carousel/Carousel";
-
 //Material UI
-import { Container } from "@mui/material";
-import Dialog from "@mui/material/Dialog";
 import { createStyles, makeStyles } from "@mui/styles";
-import Button from "@mui/material/Button";
+import { Button, Container, Dialog } from "@mui/material";
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -44,7 +41,7 @@ function HomePage() {
 
   const generateHomeDetailSummary = ({ id, images, heading, description }) => {
     return (
-      <Container maxWidth={'lg'} key={"Home Summary " + id}>
+      <Container maxWidth={"lg"} key={"Home Summary " + id}>
         <DetailSummary
           id={id}
           images={images}
@@ -83,16 +80,16 @@ function HomePage() {
       <Head>
         <title>Tralio</title>
         <link
-        rel="stylesheet"
-        type="text/css"
-        charSet="utf-8"
-        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"
-      />
-      <link
-        rel="stylesheet"
-        type="text/css"
-        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"
-      />
+          rel="stylesheet"
+          type="text/css"
+          charSet="utf-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"
+        />
       </Head>
       <TopLandingScreen />
       <Container maxWidth="lg" className={styles.increase_padding}>
@@ -114,16 +111,21 @@ function HomePage() {
         {homeDetailFeature.map(generateHomeDetailFeature)}
         <div className={styles.profBox}>
           <p className={styles.profile}>Flourish your profile online</p>
-            <Button
-                variant={'contained'}
-                onClick={handleClick}
-                sx={{
-                    padding: {lg: '10px 44px', md: '10px 44px', sm: '5px 34px', sx: '5px 27px'},
-                    background: '#1981FF',
-                }}
-            >
-                Build Now
-            </Button>
+          <Button
+            variant={"contained"}
+            onClick={handleClick}
+            sx={{
+              padding: {
+                lg: "10px 44px",
+                md: "10px 44px",
+                sm: "5px 34px",
+                sx: "5px 27px",
+              },
+              background: "#1981FF",
+            }}
+          >
+            Build Now
+          </Button>
           <Dialog
             open={pop}
             onClose={handleClose}
