5971bb0b2355d4ee91291565b0b36255f31b0686
Manisa Basak <73214790+manisa18@users.noreply.github.com>
2022-03-25 18:55:14 +0530

    Edited TinyMCE and Added Live Preview

--- pages/add-post/index.js
+++ pages/add-post/index.js
@@ -1,21 +1,28 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import Box from "@mui/material/Box";
-import { Container } from "@mui/material";
+import { Button, Container } from "@mui/material";;
+import ReactHtmlParser from "react-html-parser"
 
 export default function AddPost() {
   const editorRef = useRef(null);
-
+  const [initial, setInitial] = useState("Write Something");
+   const log = () => {
+       console.log(editorRef.current.getContent())
+     }
   return (
     <>
       <Container>
         <Box mt={10} />
         <Editor
+          apiKey="jq0hm5i9g8sxohg5nsiut5ugowize8j50790c8eu8eeajiir"
           onInit={(evt, editor) => (editorRef.current = editor)}
-          initialValue="<p>This is the initial content of the editor.</p>"
+          value={initial}
+          onEditorChange={(content) => setInitial(content)}
           init={{
             selector: "textarea",
             height: 500,
+            image_dimensions: true,
             plugins: [
               "advlist autolink lists link image charmap print preview anchor",
               "searchreplace visualblocks fullscreen",
@@ -26,7 +33,14 @@ export default function AddPost() {
             content_style:
               "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
           }}
-              />
+        />
+        <Button sx={{my:2}} variant="contained" onClick={() => {console.log(initial)}}>
+          Click
+        </Button>
+        <h2>Preview</h2>
+        <Box sx={{ overflow: "auto", height:500}}>
+          {initial && ReactHtmlParser(initial)}
+        </Box>
         <Box mt={10} />
       </Container>
     </>
