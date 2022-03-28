83ef8c6d3c9c09a3da9314ff8fd3d61f5dce4f77
Manisa Basak <73214790+manisa18@users.noreply.github.com>
2022-03-28 23:47:31 +0530

    Added Profile page

--- container/Dashboard/Profile.jsx
+++ container/Dashboard/Profile.jsx
@@ -1,39 +1,76 @@
 import { useState } from "react";
 import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
+import { Container, Avatar, Typography, Button } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
 
 export default function Profile(props) {
-  const data = [
+  const data =
     {
       id: 1,
-      username: "something",
+      username: "Something",
       firstName: "Manisa",
       lastName: "Basak",
       email: "something@g.com",
       password: "popop",
       gender: "female",
-    },
-    ];
+    };
     const [userData, setUserData] = useState({});
-
-    const findData = () => {
-        data.find(id === 1).then(se)    }
   return (
     <>
       <Box mt={8} />
-      <Container maxWidth={"lg"}>{props.details}</Container>
-      <Box mt={100} />
+      <Container maxWidth={"lg"}>
+        <Box display={"flex"}>
+          <Box
+            display={"flex"}
+            flexDirection={"column"}
+            justifyContent={"center"}
+            alignItems={"center"}>
+            <Avatar sx={{ height: 200, width: 200, fontSize: 100 }}>
+              {data.firstName[0]}
+            </Avatar>
+            <Typography variant="h5" fontSize={30} py={2}>
+              {data.firstName + " " + data.lastName}
+            </Typography>
+            <Button variant="contained" sx={{ px: 6, fontSize: 13 }} onClick={()=>{window.location.href = "/settings"}}>EDIT <EditIcon sx={{ml:1}} fontSize="small"/></Button>
+          </Box>
+          <Box p={8} width={"100%"}>
+            <Box mt={4} />
+            <Typography
+              variant="h6"
+              p={2}
+              backgroundColor="#1981FF"
+              color="white">
+              PROFILE INFORMATION
+            </Typography>
+            <Box display={"flex"} flexDirection={"row"} p={2} borderBottom={1}>
+              <Typography width={"15%"}>NAME</Typography>
+              <Typography>{data.firstName + " " + data.lastName}</Typography>
+            </Box>
+            <Box display={"flex"} flexDirection={"row"} p={2} borderBottom={1}>
+              <Typography width={"15%"}>EMAIL</Typography>
+              <Typography>{data.email}</Typography>
+            </Box>
+            <Box display={"flex"} flexDirection={"row"} p={2} borderBottom={1}>
+              <Typography width={"15%"}>GENDER</Typography>
+              <Typography>
+                {data.gender}
+              </Typography>
+            </Box>
+            <Box display={"flex"} flexDirection={"row"} p={2} borderBottom={1}>
+              <Typography width={"15%"}>NAME</Typography>
+              <Typography>
+                {data.firstName + " " + data.lastName}
+              </Typography>
+            </Box>
+            <Box display={"flex"} flexDirection={"row"} p={2} borderBottom={1}>
+              <Typography width={"15%"}>NAME</Typography>
+              <Typography>
+                {data.firstName + " " + data.lastName}
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
+      </Container>
     </>
   );
-}
-export async function getStaticProps() {
-  const data = await fetch(`${API_URL}/api/${apiVersion}/users/register`).then(
-    (res) => console.log(res.json())
-  );
-
-  return {
-    props: {
-      details: data,
-    },
-  };
 }
