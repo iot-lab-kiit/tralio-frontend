a1976d40adeaec7f1da714ad08314de3fa222eba
roshanchatei <rkc.mj5@gmail.com>
2022-04-01 20:59:14 +0530

    Grid error solved

--- container/Dashboard/UpdatePortfolioData.jsx
+++ container/Dashboard/UpdatePortfolioData.jsx
@@ -269,8 +269,8 @@ export default function UpdatePortfolioData() {
     );
   };
   return (
-    <Grid display={"flex"} justifyContent={"space-between"}>
-      <Grid xs={0} sm={3} md={2}>
+    <Grid container display={"flex"} justifyContent={"space-between"}>
+      <Grid item xs={0} sm={3} md={2}>
         <Hidden smDown>
           <Box
             mt={12}
@@ -282,7 +282,7 @@ export default function UpdatePortfolioData() {
           </Box>
         </Hidden>
       </Grid>
-      <Grid mt={8} xs={12} sm={9} md={10}>
+      <Grid item mt={8} xs={12} sm={9} md={10}>
         <Container>
           <Hidden smUp>
             <Box display={"flex"} justifyContent={"space-between"}>
