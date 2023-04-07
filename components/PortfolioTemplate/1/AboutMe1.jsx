b3dc847848ca754f9d978916afa7e7e8f769ae0d
Saptarshee Mitra <saptarshee08@gmail.com>
2023-04-07 19:13:05 +0530

    bug fix

--- components/PortfolioTemplate/1/AboutMe1.jsx
+++ components/PortfolioTemplate/1/AboutMe1.jsx
@@ -10,7 +10,8 @@ export default function AboutMe1(props) {
                 <Box textAlign={'center'} width={'100%'}>
                     <Title value={'About Me'} text={0} />
                     <Box mt={8} px={{lg: 8, md: 8, sm: 6, xs: 2}} fontSize={'20px'} fontWeight={500}>
-                        {profile.bio}
+                        {/* {profile.bio} */}
+                        Hello Test
                     </Box>
                 </Box>
             </Container>
